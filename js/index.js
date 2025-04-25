5cbae7a4d9b44450f90fe6a7a449ac9d77355643
Jungalator <papajohn.strike@gmail.com>
2025-04-25 15:12:36 +0300

    Init commit
    Add HTML, CSS, JS

---
+++ js/index.js
@@ -0,0 +1,148 @@
+"use strict";
+import { products } from "./templates/templates.js";
+
+let sum = "";
+
+const container = document.createElement("div");
+container.className = "container";
+
+const sumPriceContainer = document.createElement("div");
+sumPriceContainer.className = "sumPriceContainer";
+sumPriceContainer.textContent = sum;
+
+const messageContainer = document.createElement("div");
+
+const paramsContainer = document.createElement("div");
+paramsContainer.className = "paramsContainer";
+
+const itemContainer = document.createElement("div");
+itemContainer.className = "itemContainer";
+
+const itemList = document.createElement("ul");
+itemList.className = "itemList";
+
+const labelElectronic = document.createElement("label");
+labelElectronic.textContent = "Электроника";
+labelElectronic.style.marginRight = "20px";
+
+const electronicCategory = document.createElement("input");
+electronicCategory.type = "checkbox";
+
+const labelDress = document.createElement("label");
+labelDress.textContent = "Одежда";
+
+const dressCategory = document.createElement("input");
+dressCategory.type = "checkbox";
+
+const filterPriceForm = document.createElement("form");
+const minPrice = document.createElement("input");
+minPrice.type = "number";
+minPrice.id = "minPrice";
+minPrice.placeholder = "min price";
+minPrice.className = "price-input";
+
+const maxPrice = document.createElement("input");
+maxPrice.type = "number";
+maxPrice.id = "maxPrice";
+maxPrice.placeholder = "max price";
+maxPrice.className = "price-input";
+
+const filterPrice = document.createElement("input");
+filterPrice.type = "submit";
+
+const updateProducts = products.map((item) => ({
+  ...item,
+  price: (item.price += " 💰"),
+  category:
+    item.category === "Электроника"
+      ? (item.category += " 💻")
+      : (item.category += " 👔"),
+}));
+
+function filterProducts(event) {
+  event.preventDefault();
+
+  // Преобразуем значения цены в число
+  const minPriceValue = minPrice.value ? Number(minPrice.value) : 0;
+  const maxPriceValue = maxPrice.value ? Number(maxPrice.value) : Infinity;
+  //Проверяем, какие чекбоксы включены
+  itemList.textContent = "";
+
+  const isElectronicsChacked = electronicCategory.checked;
+  const isDressChacked = dressCategory.checked;
+
+  const filteredItems = updateProducts.filter((item) => {
+    const price = parseInt(item.price);
+
+    const matchesPrice = price >= minPriceValue && price <= maxPriceValue;
+    const matchesCategory =
+      (isElectronicsChacked && item.category.includes("Электроника")) ||
+      (isDressChacked && item.category.includes("Одежда")) ||
+      (!isElectronicsChacked && !isDressChacked);
+    return matchesPrice && matchesCategory;
+  });
+
+  filteredItems.forEach((item) => {
+    itemList.insertAdjacentHTML(
+      "afterbegin",
+      `
+  <li>${item.name} ${item.price} ${item.category}</li>
+
+        `
+    );
+  });
+
+  const someProducts = filteredItems.some(
+    (item) => parseInt(item.price) <= 1000
+  );
+  const allExpensive = filteredItems.every(
+    (item) => parseInt(item.price) > 3500
+  );
+
+  if (someProducts) {
+    messageContainer.textContent = "🎉 Есть выгодные предложения!";
+  } else if (allExpensive) {
+    messageContainer.textContent = "💎 Только премиум-товары!";
+  } else {
+    messageContainer.textContent = "📌 Есть товары в среднем ценовом сегменте!";
+  }
+
+  const sumPrice = filteredItems.reduce((acc, item) => {
+    return acc + parseInt(item.price);
+  }, 0);
+  sum = 0;
+  sumPriceContainer.textContent = `Сумма: ${(sum += sumPrice)} 💰`;
+}
+
+filterPrice.addEventListener("click", filterProducts);
+electronicCategory.addEventListener("change", filterProducts);
+dressCategory.addEventListener("change", filterProducts);
+
+function renderItems() {
+  updateProducts.forEach((item) => {
+    if (Number(minPrice.value) === 0 && Number(maxPrice.value) === 0) {
+      itemList.insertAdjacentHTML(
+        "afterbegin",
+        `
+    <li>${item.name} ${item.price}   ${item.category}</li>
+
+    `
+      );
+    }
+  });
+}
+
+renderItems();
+
+document.body.appendChild(container);
+container.append(
+  paramsContainer,
+  itemContainer,
+  sumPriceContainer,
+  messageContainer
+);
+itemContainer.append(itemList);
+labelElectronic.append(electronicCategory);
+labelDress.append(dressCategory);
+paramsContainer.append(filterPriceForm, labelElectronic, labelDress);
+filterPriceForm.append(minPrice, maxPrice, filterPrice);
---
+++ js/templates/templates.js
@@ -0,0 +1,11 @@
+export const products = [
+  { name: "Ноутбук", price: 50_000, category: "Электроника" },
+  { name: "Смартфон", price: 30_000, category: "Электроника" },
+  { name: "Футболка", price: 700, category: "Одежда" },
+  { name: "Кроссовки", price: 10_000, category: "Одежда" },
+  { name: "Куртка", price: 3000, category: "Одежда" },
+  { name: "Свитшот", price: 1000, category: "Одежда" },
+  { name: "Планшет", price: 20_000, category: "Электроника" },
+  { name: "Наушники", price: 3000, category: "Электроника" },
+  { name: "Часы", price: 10_000, category: "Электроника" },
+];
